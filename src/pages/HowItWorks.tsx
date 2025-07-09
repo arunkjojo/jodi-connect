86ff3c54b3586315a42be26c0605b96762463e99
Arun Jojo <42526032+arunkjojo@users.noreply.github.com>
2025-07-09 23:09:50 +0530

    Updated HowItWorks.tsx

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -63,7 +63,7 @@ const HowItWorks: React.FC = () => {
         </div>
       </div>
 
-      <div className="px-4 py-8">
+      <div className="px-4 pt-8 pb-20">
         <div className="max-w-6xl mx-auto">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 sm:gap-6">
           {steps.map((step) => (
