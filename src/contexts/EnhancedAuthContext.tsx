33dced9af20a8f5e3361f87d0b3052e3b4174c73
Inluxi <155431610+inluxi@users.noreply.github.com>
2025-07-13 15:06:11 +0530

    Updated EnhancedAuthContext.tsx

--- src/contexts/EnhancedAuthContext.tsx
+++ src/contexts/EnhancedAuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from 'react';
 import type { EnhancedAuthContextType } from '../types';
 
-const EnhancedAuthContext = createContext<EnhancedAuthContextType | undefined>(undefined);
+export const EnhancedAuthContext = createContext<EnhancedAuthContextType | undefined>(undefined);
 
 export const useEnhancedAuth = () => {
   const context = useContext(EnhancedAuthContext);
